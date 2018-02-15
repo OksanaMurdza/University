7c59a2a4d9dd2205f9b327423ac490c6d028921a
igoreshageser <igorluna@bigmir.net>
2018-02-15 10:30:56 +0200

    add toggle

--- web-design/lab3/client/script.js
+++ web-design/lab3/client/script.js
@@ -1,26 +1,32 @@
 import $ from "jquery";
 import { takeToDoList } from "../api";
 
+const nearDeal = "";
+
 $(() => {
-  takeToDoList().then(list => buildList(list));
+  takeToDoList()
+    .then(list => sortDealTime(list))
+    .then(list => buildList(list));
 });
 
 function buildList(list) {
   const dealDIV = $("#deal");
   const num = $(".deal-num");
-  const content = $(".deal-conent");
   list.forEach((item, index) => {
     const newNode = createBlock(...item, index);
     dealDIV.append(newNode);
   });
+  const content = Object.values($(".deal-content"));
 
-  Object.values(num).forEach((item, index) => $(item).click(() => {}));
+  Object.values(num).forEach((item, index) =>
+    $(item).click(e => $(content[e.target.id]).toggle())
+  );
 }
 
 function createBlock(time, deal, descr, num) {
   return `
   <div class= "deal-item">
-  <div class="deal-num">
+  <div class="deal-num" id="${num}">
   ${num}
 </div>
 <div class="deal-content"> <div class="deal-time">
@@ -42,4 +48,17 @@ function createBlock(time, deal, descr, num) {
     </p>
 </div></div>
 </div>`;
+}
+
+const prettyView = string => +string.replace(":", "");
+
+// hardcode ;c
+function sortDealTime(list) {
+  return list.sort((a, b) => {
+    if (prettyView(a[0]) > prettyView(b[0])) return 1;
+
+    if (prettyView(a[0]) < prettyView(b[0])) return -1;
+
+    return 0;
+  });
 }
