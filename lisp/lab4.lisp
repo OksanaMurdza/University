9751d2531d981d44e22990fcf0073a31c7cf9b01
igor.misyachniy <igorluna@bigmir.net>
2018-03-08 19:06:07 +0200

    lisp / lab4

---
+++ lisp/lab4.lisp
@@ -0,0 +1,33 @@
+(setq set '(a b c d e f g p))
+(defvar i 1)
+(setq buffer '())
+
+(defun newList (lst)
+    (when lst
+        (if (= i 3)
+            (setq i 1)
+            (progn 
+                (incf i)
+                (setq head (car lst))
+                (push head buffer)
+            )
+        )
+        (setq tail (cdr lst))
+        (newList tail)
+    )
+    (setq buffer (reverse buffer))
+)
+
+(newList set)
+(print buffer)
+
+ 
+  
+  
+
+
+
+
+
+
+
