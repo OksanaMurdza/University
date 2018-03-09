28557fd2f6afa2400b6700418ad9f533dfafdeb2
igor.misyachniy <igorluna@bigmir.net>
2018-03-09 11:20:52 +0200

    list / lab6

--- lisp/lab6.lisp
+++ lisp/lab6.lisp
@@ -1,6 +1,6 @@
 (let ((firstLst '(1 2 3 4 5)) (secondLst '(2 4 6 5 8 10)))
-    (defvar concatLst (append firstLst secondLst))
-)
+    (defvar concatLst (append firstLst secondLst)))
+
 
 (defvar sum 0)
 
@@ -8,16 +8,15 @@
 (defun sumSet (lst)
     (when lst
         (let ((head (car lst)))
-        (let ((a ((lambda (x) (mod x 2)) head)))
-            (if (= a 0)
-                (incf sum head))
-            (let ((tail (cdr lst)))
-            (sumSet tail))
-        ))
-    )
-    (return-from sumSet sum)
-)
+         (let ((a ((lambda (x) (mod x 2)) head)))
+             (if (= a 0)
+                 (incf sum head))
+             (let ((tail (cdr lst)))
+              (sumSet tail)))))
+        
+    
+    (return-from sumSet sum))
+
 
 (let ((res (sumSet concatLst)))
-    (print res)
-)
+    (print res))
