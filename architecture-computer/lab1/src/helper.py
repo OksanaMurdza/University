8ff5b064ba15dbbffb5dd2ef6ee11af8a9723acc
FowiFlowi <fowiflowi@gmail.com>
2018-04-17 11:40:24 +0300

    fixed codestyle with pep8

--- architecture-computer/lab1/src/helper.py
+++ architecture-computer/lab1/src/helper.py
@@ -4,17 +4,18 @@ import os
 import feedparser
 import re
 
+
 def read_xml_file():
-  tree = ET.parse('url.xml')
-  root = tree.getroot()
+    tree = ET.parse('url.xml')
+    root = tree.getroot()
 
-  parsed_url = []
+    parsed_url = []
 
-  for child in root:
-    buff = child.attrib
-    parsed_url.append(buff["url"])
-  
-  return parsed_url
+    for child in root:
+        buff = child.attrib
+        parsed_url.append(buff["url"])
+
+    return parsed_url
 
 
 def score_process(string):
@@ -22,29 +23,31 @@ def score_process(string):
     matches = re.finditer(regex, string)
     res = ''
     for matchNum, match in enumerate(matches):
-      res = match.group()
+        res = match.group()
     return res
 
 
 def rss_parse(url):
-  d = feedparser.parse(url)
-  news = d['entries']
-  res = []
+    d = feedparser.parse(url)
+    news = d['entries']
+    res = []
 
-  for new in news:
-    news_tags = [new['title'], new['description']]
-    for tags in news_tags:
-      parsed = score_process(tags)
-      if parsed != '':
-        res.append(parsed)
-  return res
+    for new in news:
+        news_tags = [new['title'], new['description']]
+        for tags in news_tags:
+            parsed = score_process(tags)
+            if parsed != '':
+                res.append(parsed)
+    return res
+
 
 def print_to_file(fileName, data, mode):
-  fh = open(fileName, mode)
-  fh.write(data)
-  fh.write('\n')  
-  fh.close()
-  return data
+    fh = open(fileName, mode)
+    fh.write(data)
+    fh.write('\n')
+    fh.close()
+    return data
+
 
 def del_file(fileName):
-  os.remove("./" + fileName)
+    os.remove("./" + fileName)
--- architecture-computer/lab1/src/index.py
+++ architecture-computer/lab1/src/index.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 from helper import read_xml_file, rss_parse, print_to_file, del_file
 from dicttoxml import dicttoxml
@@ -6,14 +6,14 @@ from dicttoxml import dicttoxml
 fileName = "result.xml"
 
 try:
-  open(fileName)
+    open(fileName)
 except IOError:
-  print 'This file not exist: {}'.format(fileName)
+    print 'This file not exist: {}'.format(fileName)
 else:
-  del_file(fileName)
-        
+    del_file(fileName)
+
 rss_urls = read_xml_file()
 
 for url in rss_urls:
-    xml = dicttoxml(rss_parse(url), custom_root = 'test', attr_type = False)
+    xml = dicttoxml(rss_parse(url), custom_root='test', attr_type=False)
     print_to_file(fileName, xml, "a")
