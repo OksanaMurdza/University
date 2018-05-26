3efba14c5062981b2297972b69a735817a419c2e
igoreshageser <igorluna@bigmir.net>
2018-05-26 15:36:18 +0300

    IPZ // code generator first step - ready

--- IPZ/code_generator/code_generator.py
+++ IPZ/code_generator/code_generator.py
@@ -1,8 +1,9 @@
 from tree_struct import get_gramm_rule
 
 labels_store = []
-labels_flag = True
-labels_id = 1
+condition = []
+semaphore = False
+
 
 
 def error_handle(f):
@@ -18,6 +19,7 @@ def error_handle(f):
 
 def create_label(name, goto):
   global labels_id
+  global semaphore
 
   label_name = '{}_{}'.format(name, labels_id)
   labels_id += 1
@@ -28,180 +30,147 @@ def create_label(name, goto):
   }
   
 
-@error_handle
 def code_gen(ast):
   global labels_store
-  global labels_flag
+  global semaphore
 
   res = ast.get_node()
   rule = res['rule']
 
-
-  # <signal_program> || <declarations> 
-  if rule in [1, 4]:
+  # signal-program
+  if rule == 1:
     code_gen(res['child'][0])
 
-  # <program>
+  # program
   if rule == 2:
-    # -> procedure_identifier
-    proc_ident = code_gen(res['child'][1])
-    print 'SEGMENT {}'.format(proc_ident)
-    # -> block
-    code_gen(res['child'][2])
+    # procedure-identifier
+     procedure_identifier = code_gen(res['child'][1])
+     code_gen(res['child'][2])
 
-  
-  # <block>
+  # block
   if rule == 3:
-    # -> declarations
     code_gen(res['child'][0])
-    # -> statements_list    
     code_gen(res['child'][2])
 
-  # <label_declarations>
+  # declarations
+  if rule == 4:
+    code_gen(res['child'][0])
+
+  # label_declarations
   if rule == 5:
-    # -> unsigned_integer
-    code_gen(res['child'][1])
-    # -> label_list
+    unsig_int = code_gen(res['child'][1])
+
+    # label-list
     code_gen(res['child'][2])
 
-  # <unsigned_integer>
+  # unsigned_int 
   if rule == 6:
-    child = get_child(res)
-    if labels_flag:
-      labels_store.append(child['value']['lexem'])
-    else:
-      return child['value']['lexem']
+    unsig_int = get_child(res)
+    return unsig_int['value']['lexem']
 
-  # <label_list>
+  # label_list
   if rule == 7:
-    child = get_child(res)
+    child = res['child'][0].get_node()
     if child['value'] != '<empty>':
-      # -> unsigned_integer
-      code_gen(res['child'][1])
-      # -> label-list
+      unsig = code_gen(res['child'][1])
+      labels_store.append(unsig)
       code_gen(res['child'][2])
-    elif child['value'] == '<empty>':
-      code_gen(res['child'][0])
-      labels_flag = False
 
-  # statements_list
+  # statement-list
   if rule == 8:
-    # statement
-    unsigned = code_gen(res['child'][0])
-    code_gen(res['child'][1])
+    child = res['child'][0].get_node()
+    if child['value'] != '<empty>':
+      # statemnt
+      result = code_gen(res['child'][0])
+      return result
+      
+    return result
 
-    return unsigned
-
-  
-  # <statement>
+  # statement
   if rule == 9:
     child = get_child(res)
-    unsig = ''
+
     if child['value'] == '<unsigned_integer>':
-      # -> unsigned_integer
-      unsigned = code_gen(res['child'][0])
-      # -> statement
-      statement = code_gen(res['child'][2])
-      if statement:
-        unsig = [unsigned, statement]
-    # -> GOTO
-    elif type(child['value']) is dict:
-      if child['value']['lexem'] == 'GOTO':
-        unsig = code_gen(res['child'][1])
-
+      label = code_gen(res['child'][0])
+      pair(label)
+      res = code_gen(res['child'][2])
+      return res
+    
     elif child['value'] == '<condition_statement>':
-      unsig = code_gen(res['child'][0])
+      code_gen(res['child'][0])
 
-    return unsig
+    elif child['value']['lexem'] == 'GOTO':
+      goto_label = code_gen(res['child'][1])
+      condition_arr = pair(goto_label)
+      return condition_arr
+    
 
-  # <condition_statement>
+  # condition_statement
   if rule == 10:
-    # -> incomplite_condition_statement
-    if_branch = code_gen(res['child'][0]) 
+    # incomplite_condition_statement
+    code_gen(res['child'][0])
 
+    # alternative_part
+    alternative = code_gen(res['child'][1])
+    print alternative
 
-    # -> alternative_part
-    else_branch = code_gen(res['child'][1])
-
-    if_label = create_label('if', if_branch[1])
-
-    if type (else_branch) is list:
-      else_label = create_label('else', else_branch[1])
-
-      print "JNE {}".format(else_label['label_name'])
-      print "JMP {}".format(if_label['label_name'])
-
-      print if_label['label_content']
-      print else_label['label_content']
-    else:
-      print "JMP {}".format(if_label['label_name'])
-      print if_label['label_content']
-
-
-  # <incomplite_condition_statement>
+  # incomplite_condition_statement
   if rule == 11:
-    # -> condition_expression
-    code_gen(res['child'][1])
-
-    # -> statement_list
-    res = code_gen(res['child'][3])
-    
-    if type(res) is list:
-      for label in res:
-        if not label in labels_store:
-          raise Exception('NOT_DEFINED_LABEL')
+    # condition_expression
+    condition = code_gen(res['child'][1])
+    semaphore = True
+    # statement-list
+    sec_child = code_gen(res['child'][3])
+    print condition
+    print sec_child
 
     
-    # else
-    # print 'JMP ?{}'.format(res[0])
-
-    # print 'incom{}'.format(res)
-    return res
-
-  # <condition_expression> 
+  # condition_expression
   if rule == 12:
-    # -> variable_identifier
-    var_iden = code_gen(res['child'][0])
-    # -> unsigned_integer
-    unsig_int = code_gen(res['child'][2])
+    variable = code_gen(res['child'][0])
+    equal = code_gen(res['child'][2])
 
-    # print 'labels_store  ->{}'.format(labels_store)
-    print 'cmp {}, {}'.format(var_iden, unsig_int)
-    # print 'JNE else_label'
-    # print 'next deal'
+    return [variable, equal]    
 
-  # <variable_identifier>
+  # variable_identifier
   if rule == 13:
-    ident = code_gen(res['child'][0])
+    return code_gen(res['child'][0])
+    
 
-    return ident
-
-  # <identifier>
+  # identifier
   if rule == 14:
-    child = get_child(res)
-    ident = child['value']['lexem']
+    ident = get_child(res)
+    return ident['value']['lexem']
+    
+  # procedure-identifier
+  if rule == 15:
+    # identifier
+    ident = code_gen(res['child'][0])
+    
     return ident
 
-  # <alternative_part>
+  # alternative_part
   if rule == 16:
-    res = code_gen(res['child'][1])
+    child = res['child'][0].get_node()
+    if child['value'] != '<empty>':
+      return code_gen(res['child'][1])
 
-    return res
-
-  if rule == 0:
-    pass
-
-    return ident
-
-  # <alternative_part>
-  if rule == 16:
-    res = code_gen(res['child'][1])
-
-    return res
-
-  if rule == 0:
-    pass
-
+  # print res
 
 def get_child(node):
   return node['child'][0].get_node()
+
+def pair(var):
+  global condition
+  global semaphore
+
+  if not semaphore:
+    return
+
+  if len(condition) < 2:
+    condition.append(var)
+  
+  if len(condition) == 2:
+    buff = condition
+    condition = [] 
+    return buff
