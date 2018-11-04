dee0a517d1d9e1e60b0e8e636537599f7f7373fa
igor.misyachniy <igor.misyachniy@gns-it.com>
2018-11-04 17:29:23 +0200

    RR | SJF

---
+++ OS/lab1/2var/RR.js
@@ -0,0 +1,118 @@
+const { generatorCreator } = require("../utils");
+const quietMode = false;
+
+class RoundRobin {
+  constructor(process, time, queueNumber) {
+    this.process = process;
+    this.quant = time;
+    this.processInstace = [];
+    this.queueNumber = queueNumber;
+
+    this.countTime = 0;
+    this.currentProcess = 0;
+
+    this.mainInterval = null;
+  }
+
+  changeCurrentProcess() {
+    let newProcess;
+
+    // takoe shoto :c
+    // take next not finished process
+    this.process.forEach(({ status }, index) => {
+      if (status || newProcess) return;
+
+      if (index > this.currentProcess) {
+        newProcess = index;
+      }
+    });
+
+    // take first not finished process
+    if (!newProcess) {
+      this.process.forEach((item, index) => {
+        if (newProcess >= 0) return;
+
+        if (!item.status) {
+          newProcess = index;
+        }
+      });
+    }
+
+    this.currentProcess = newProcess;
+  }
+
+  fillProcess() {
+    this.processInstace = this.process.map(({ time }, index) =>
+      generatorCreator(time, index)
+    );
+  }
+
+  roundCircle() {
+    if (!this.notFinishedProcess.length) {
+      this.finish();
+      return Promise.resolve([]);
+    }
+
+    if (this.countTime === this.quant) {
+      this.countTime = 1;
+      if (this.isCurrentProcessIsFinished) {
+        this.changeCurrentProcess();
+      } else {
+        const notFinishedProcess = this.process[this.currentProcess];
+        return Promise.resolve(notFinishedProcess);
+      }
+    }
+
+    this.countTime++;
+
+    const process = this.processInstace[this.currentProcess];
+    const result = process.next();
+    // this.logger(result);
+    if (result.done) {
+      this.process[this.currentProcess]["status"] = true;
+      this.changeCurrentProcess();
+    }
+
+    return Promise.resolve(null);
+  }
+
+  logger(text) {
+    if (!quietMode && text !== "finish") {
+      console.log(`${JSON.stringify(text)}   RR | Queue ${this.queueNumber}`);
+    } else {
+      console.log("************");
+      console.log(`Queue ${this.queueNumber}`);
+      console.log(this.process);
+      console.log("************");
+    }
+  }
+
+  start() {
+    this.fillProcess();
+    this.mainInterval = setInterval(() => {
+      this.roundCircle().then(d => {
+        if (d !== null) {
+          Promise.resolve(d);
+        }
+      });
+    }, this.quant);
+    return Promise.resolve([]);
+  }
+
+  finish() {
+    clearInterval(this.mainInterval);
+    return this.notFinishedProcess;
+  }
+
+  get isCurrentProcessIsFinished() {
+    const { status } = this.process[this.currentProcess];
+    return status;
+  }
+
+  // change to return GENERATOR
+  get notFinishedProcess() {
+    return this.process.filter(({ status }) => !status);
+  }
+}
+
+module.exports = RoundRobin;
---
+++ OS/lab1/2var/index.js
@@ -0,0 +1,144 @@
+const { createProcess, generatorCreator } = require("../utils");
+
+let PROCESS = [10, 30, 30, 31, 33, 32, 34, 14, 14, 11, 10, 40, 50];
+const QUANT = 20;
+
+class RR {
+  constructor(process, time, id) {
+    this.process = process;
+    this.time = time;
+    this.id = id;
+
+    this.processSyncFlag = true;
+    this.bufferNewProcess = [];
+  }
+
+  addNewProcess(newProcess) {
+    const { id, processSyncFlag } = this;
+
+    if (!newProcess.length) return;
+
+    if (!processSyncFlag) {
+      setTimeout(() => this.addNewProcess(newProcess), 0);
+      return;
+    }
+    this.process = [...newProcess];
+
+    this.t().then(p => {
+      console.log(id, " * ", p);
+      switch (id) {
+        case 1:
+          r2.addNewProcess(p);
+          break;
+        case 2:
+          sjf.addNewProcess(p);
+          break;
+      }
+    });
+  }
+
+  async t() {
+    const buff = [];
+    this.processSyncFlag = false;
+    const processLen = this.process.length;
+    for (let i = 0; i < processLen; i++) {
+      const r = await this.execute(this.getNewProcess);
+      buff.push(r);
+    }
+    this.process = [];
+    this.processSyncFlag = true;
+    return buff.filter(process => process);
+  }
+
+  execute(ms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.time >= ms ? resolve(null) : resolve(ms);
+      }, this.time);
+    });
+  }
+
+  get getNewProcess() {
+    const [p] = this.process;
+    this.process.shift();
+    return p;
+  }
+}
+
+class SJF {
+  constructor(process) {
+    this.process = process;
+
+    this.processSyncBuffer = [];
+    this.processSyncFlag = true;
+  }
+
+  addNewProcess(newProcess) {
+    if (!newProcess.length) return;
+
+    if (!this.processSyncFlag) {
+      console.log("asd");
+      this.processSyncBuffer = [...newProcess];
+      return;
+    }
+
+    this.process = [...newProcess];
+    this.t().then(d => {
+      if (d.length) {
+        console.log("sjf ", d);
+      }
+    });
+  }
+
+  execute(ms) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(ms), ms);
+    });
+  }
+
+  async t() {
+    this.processSyncFlag = false;
+    const processLen = this.process.length;
+    const buff = [];
+    for (let i = 0; i < processLen; i++) {
+      const { process } = this.getNewProcess;
+      const r = await this.execute(process);
+      if (this.processSyncBuffer.length) {
+        console.log("asd");
+      }
+      buff.push(r);
+    }
+    this.processSyncFlag = true;
+    return buff;
+  }
+
+  get getNewProcess() {
+    let min = {
+      process: this.process[0],
+      index: 0
+    };
+
+    this.process.forEach((item, index) => {
+      if (item < min) {
+        min = {
+          process: item,
+          index: index
+        };
+      }
+    });
+
+    this.process = this.process.filter((_, index) => !index !== min.index);
+
+    return min;
+  }
+}
+
+const sjf = new SJF([]);
+const r2 = new RR([], QUANT + 15, 2);
+const rr = new RR([], QUANT, 1);
+
+rr.addNewProcess(PROCESS);
+
+setTimeout(() => {
+  rr.addNewProcess([100]);
+}, QUANT + 20);
