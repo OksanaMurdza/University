4d6ece14d7ec685f9fdb3c97d5fcead6c2b18766
igoreshageser <igorluna@bigmir.net>
2018-11-07 21:45:44 +0200

    mda

---
+++ sys-program/lab1/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysmacros.h>
+
+int main(void)
+{
+  printf("test");
+  return 0;
+}
