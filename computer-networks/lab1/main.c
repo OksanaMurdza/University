8253c9a33cfeca65c3364017d5bf634b779a4b90
igoreshageser <igorluna@bigmir.net>
2018-11-07 20:34:01 +0200

    add CN labs

---
+++ computer-networks/lab1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("\n It works \n");
+  return 0;
+}
---
+++ computer-networks/lab1/secondTest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("\n It works 2 \n");
+  return 0;
+}
